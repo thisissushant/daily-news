8341453426ebb709fd36a1295074e136ea38ad7f
Sushant Singh <sushantbibhu@gmail.com>
2023-08-01 14:00:59 +0530

    update-NabBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export default class NavBar extends Component {
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
             <a class="navbar-brand" href="/#">
-              Navbar
+              Daily News
             </a>
             <button
               class="navbar-toggler"
@@ -29,53 +29,11 @@ export default class NavBar extends Component {
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="/#">
-                    Link
+                    About
                   </a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link dropdown-toggle"
-                    href="/#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a class="dropdown-item" href="/#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
                 </li>
                 <li class="nav-item"></li>
               </ul>
-              <form class="d-flex" role="search">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button class="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
             </div>
           </div>
         </nav>
