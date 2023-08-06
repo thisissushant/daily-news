a9fc399b160fc6bfeb3e9d1ff1a651bdc8b0452a
Sushant Singh <sushantbibhu@gmail.com>
2023-08-06 22:55:13 +0530

    center-text

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ export default class NavBar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-center">
           <div className="container-fluid">
             <a className="navbar-brand" href="/#">
               Daily News
