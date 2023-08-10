b08c5a43153f37c4f4c6053dafc575ef551772d3
Sushant Singh <sushantbibhu@gmail.com>
2023-08-10 11:53:26 +0530

    add-GMT-Object

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -18,10 +18,11 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <p class="card-text">
-              <small class="text-body-secondary">
+            <p className="card-text">
+              <small className="text-body-secondary">
                 {" "}
-                By {author} on {date} 3 mins ago
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
               </small>
             </p>
             <a
