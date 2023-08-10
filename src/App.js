3520956266eda21e4fcaea336de08d4cdf0eb05c
Sushant Singh <sushantbibhu@gmail.com>
2023-08-10 11:23:32 +0530

    fix-error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default class App extends Component {
   render() {
@@ -11,54 +11,96 @@ export default class App extends Component {
       <div>
         <Router>
           <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <News
-                key="general"
-                pageSize={5}
-                country="in"
-                category="general"
-              />
-            </Route>
-            <Route exact path="/business">
-              <News
-                key="business"
-                pageSize={5}
-                country="in"
-                category="business"
-              />
-            </Route>
-            <Route exact path="/entertainment">
-              <News
-                key="entertainment"
-                pageSize={5}
-                country="in"
-                category="entertainment"
-              />
-            </Route>
-            <Route exact path="/health">
-              <News key="health" pageSize={5} country="in" category="health" />
-            </Route>
-            <Route exact path="/science">
-              <News
-                key="science"
-                pageSize={5}
-                country="in"
-                category="science"
-              />
-            </Route>
-            <Route exact path="/sports">
-              <News key="sports" pageSize={5} country="in" category="sports" />
-            </Route>
-            <Route exact path="/technology">
-              <News
-                key="technology"
-                pageSize={5}
-                country="in"
-                category="technology"
-              />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  key="general"
+                  pageSize={8}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              path="/Business"
+              element={
+                <News
+                  key="business"
+                  pageSize={8}
+                  country="in"
+                  category="business"
+                />
+              }
+            ></Route>
+            <Route
+              path="/Entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={8}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              path="/General"
+              element={
+                <News
+                  key="general"
+                  pageSize={8}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              path="/Health"
+              element={
+                <News
+                  key="health"
+                  pageSize={8}
+                  country="in"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              path="/Science"
+              element={
+                <News
+                  key="science"
+                  pageSize={8}
+                  country="in"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              path="/Sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={8}
+                  country="in"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              path="/Technology"
+              element={
+                <News
+                  key="technology}>"
+                  pageSize={8}
+                  country="in"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
         </Router>
       </div>
     );
