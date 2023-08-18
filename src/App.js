1388c193d962b60509f7590502e47d4bd0ff0a7f
Sushant Singh <sushantbibhu@gmail.com>
2023-08-18 11:25:00 +0530

    Updated App.js to use useState hook for progress state.

--- src/App.js
+++ src/App.js
@@ -1,129 +1,121 @@
 import "./App.css";
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
-export default class App extends Component {
-  pageSize = 9;
-  state = {
-    progress: 0,
-  };
-  setProgress = (progress) => {
-    this.setState({ progress: progress });
-  };
-  render() {
-    return (
-      <div>
-        <Router>
-          <NavBar />
-          <LoadingBar
-            height={3}
-            color=" #66ff66"
-            progress={this.state.progress}
-          />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="general"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="general"
-                />
-              }
-            ></Route>
-            <Route
-              path="/Business"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="business"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="business"
-                />
-              }
-            ></Route>
-            <Route
-              path="/Entertainment"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="entertainment"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="entertainment"
-                />
-              }
-            ></Route>
-            <Route
-              path="/General"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="general"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="general"
-                />
-              }
-            ></Route>
-            <Route
-              path="/Health"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="health"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="health"
-                />
-              }
-            ></Route>
-            <Route
-              path="/Science"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="science"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="science"
-                />
-              }
-            ></Route>
-            <Route
-              path="/Sports"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="sports"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="sports"
-                />
-              }
-            ></Route>
-            <Route
-              path="/Technology"
-              element={
-                <News
-                  setProgress={this.setProgress}
-                  key="technology}>"
-                  pageSize={this.pageSize}
-                  country="in"
-                  category="technology"
-                />
-              }
-            ></Route>
-          </Routes>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const pageSize = 9;
+
+  const [progress, setProgress] = useState(0);
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <LoadingBar height={3} color=" #66ff66" progress={progress} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                pageSize={pageSize}
+                country="in"
+                category="general"
+              />
+            }
+          ></Route>
+          <Route
+            path="/Business"
+            element={
+              <News
+                setProgress={setProgress}
+                key="business"
+                pageSize={pageSize}
+                country="in"
+                category="business"
+              />
+            }
+          ></Route>
+          <Route
+            path="/Entertainment"
+            element={
+              <News
+                setProgress={setProgress}
+                key="entertainment"
+                pageSize={pageSize}
+                country="in"
+                category="entertainment"
+              />
+            }
+          ></Route>
+          <Route
+            path="/General"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                pageSize={pageSize}
+                country="in"
+                category="general"
+              />
+            }
+          ></Route>
+          <Route
+            path="/Health"
+            element={
+              <News
+                setProgress={setProgress}
+                key="health"
+                pageSize={pageSize}
+                country="in"
+                category="health"
+              />
+            }
+          ></Route>
+          <Route
+            path="/Science"
+            element={
+              <News
+                setProgress={setProgress}
+                key="science"
+                pageSize={pageSize}
+                country="in"
+                category="science"
+              />
+            }
+          ></Route>
+          <Route
+            path="/Sports"
+            element={
+              <News
+                setProgress={setProgress}
+                key="sports"
+                pageSize={pageSize}
+                country="in"
+                category="sports"
+              />
+            }
+          ></Route>
+          <Route
+            path="/Technology"
+            element={
+              <News
+                setProgress={setProgress}
+                key="technology}>"
+                pageSize={pageSize}
+                country="in"
+                category="technology"
+              />
+            }
+          ></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
