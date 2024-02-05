a6da50ee9ca74d68fc5a87ac29203e30956b4f2a
Sushant Singh <sushantbibhu@gmail.com>
2024-02-05 22:50:14 +0530

    Change date format to `toLocaleDateString`

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -16,7 +16,7 @@ const NewsItem = (props) => {
               ? "https://www.hindustantimes.com/ht-img/img/2023/08/01/1600x900/Nationalist-Congress-Party-leaders-Ajit-Pawar--lef_1690868092734.jpg"
               : imageUrl
           }
-          className="card-img-top h-100 w-100"
+          className="card-img-top h-100 w-100 over"
           alt="..."
         />
         <div className="card-body">
@@ -26,7 +26,7 @@ const NewsItem = (props) => {
             <small className="text-body-secondary">
               {" "}
               By {!author ? "Unknown" : author} on{" "}
-              {new Date(date).toGMTString()}
+              {new Date(date).toLocaleDateString()}
             </small>
           </p>
           <a
